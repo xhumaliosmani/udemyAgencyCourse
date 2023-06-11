125431ba47fabb34f566659e80e356210d492ff0
Djumali Osmani <xhumaliosmani@outlook.com>
2023-06-11 15:29:05 +0200

    first commit

--- src/Components/Pages/About/About.jsx
+++ src/Components/Pages/About/About.jsx
@@ -1,13 +1,129 @@
-import React from 'react'
-import Navbar from '../../Navigation/Navbar'
+import React, { useEffect, useRef } from "react";
+import Navbar from "../../Navigation/Navbar";
+import Footer from "../../Footer/Footer";
+import { useAnimation, useInView, motion } from "framer-motion";
+import TeamImage from '../../../images/Team/team1.png'
 
 const About = () => {
+    const { innerWidth: Width } = window;
+    const ref = useRef();
+    const isInView = useInView(ref);
+    const mainControlls = useAnimation();
+    useEffect(() => {
+      if (isInView) {
+        mainControlls.start("visible");
+      }
+    }, [isInView]);
   return (
     <div>
-    <Navbar/>
-    <h1 className='mt-10'>about</h1>
+      <Navbar />
+      <div className="max-w-screen overflow-hidden font-poppins">
+        <div className="sm:h-screen h-[80rem] -mb-20 sm:mb-0 bg-gradient-to-b from-gray-100/10 to-gray-200/50 pt-20">
+          <div className="mt-20">
+            <motion.div
+              ref={ref}
+              variants={{
+                hidden: { opacity: 0, y: -75 },
+                visible: { opacity: 1, y: 0 },
+              }}
+              initial="hidden"
+              animate={mainControlls}
+              transition={{ duration: 0.5, delay: 0.25 }}
+              className="flex justify-center text-center"
+            >
+              <h1 className="flex justify-center text-transparent bg-clip-text bg-gradient-to-r from-blue-500 to-emerald-500 font-extrabold text-[3rem]">
+                Who are wee and whay are wee so good ?
+              </h1>
+            </motion.div>
+            <div className="sm:flex sm:justify-between sm:mr-[10%] sm:mt-20 mt-10 ml-10">
+              <motion.div
+                ref={ref}
+                variants={{
+                  hidden: { opacity: 0, x: -75 },
+                  visible: { opacity: 1, x: 0 },
+                }}
+                initial="hidden"
+                animate={mainControlls}
+                transition={{ duration: 0.5, delay: 0.25 }}
+                className="sm:w-[50%] w-[90%] mb-10 sm:mb-0"
+              >
+                <img
+                  alt="imageAbout"
+                  src={TeamImage}
+                  className="rounded-2xl sm:toas:translate-x-[200px] sm:taos:opacity-0 sm:hover:-translate-y-2 sm:transition sm:ease-in-out duration-300 sm:hover:drop-shadow-xl sm:z-20 sm:mx-auto shadow-lg "
+                />
+              </motion.div>
+              <motion.div
+                ref={ref}
+                variants={{
+                  hidden: { opacity: 0, x: 75 },
+                  visible: { opacity: 1, x: 0 },
+                }}
+                initial="hidden"
+                animate={mainControlls}
+                transition={{ duration: 0.5, delay: 0.25 }}
+                className="sm:flex sm:justify-center text-center sm:mt-20"
+              >
+                <h1 className="text-xl sm:text-4xl sm:font-extrabold font-bold text-blue-500 ml-[-10%] sm:ml-0 mb-10 sm:mb-0">
+                  "Wee are Inspired by Results and driven by Data"
+                </h1>
+              </motion.div>
+            </div>
+            <motion.div
+              ref={ref}
+              variants={{
+                hidden: { opacity: 0 },
+                visible: { opacity: 1 },
+              }}
+              initial="hidden"
+              animate={mainControlls}
+              transition={{ duration: 0.5, delay: 0.25 }}
+              className="block h-screen sm:justify-start justify-center text-center sm:text-start sm:ml-20 ml-0"
+            >
+              <p className="sm:flex justify-center text-3xl hidden ml-[-80%] mb-[-10%] sm:my-24">
+                About Us:
+              </p>
+              <p className="hidden sm:flex border-y-2 py-4 border-y-indigo-900/30 font-semibold sm:ml-[7%] w-[70%] ">
+                Welcome to our site, where we specialize in empowering
+                businesses to unlock their true potential and achieve
+                unparalleled growth. With our expertise in advanced technology
+                solutions, we provide the tools and knowledge necessary for
+                organizations to make data-driven decisions, optimize
+                operations, and drive innovation. Our personalized approach
+                ensures that each business receives tailored solutions designed
+                to address their specific objectives and challenges. By
+                leveraging the latest advancements and continuous innovation, we
+                pave the way for businesses to thrive in today's ever-evolving
+                landscape. At our site, we believe that success lies in
+                harnessing the power of technology and data to drive meaningful
+                outcomes. Whether you're a small startup or a large enterprise,
+                our comprehensive range of services and solutions is designed to
+                meet your unique needs. From advanced algorithms and powerful
+                analytics capabilities to seamless technology integration and
+                ongoing support from our trained personnel, we are dedicated to
+                helping businesses grow and succeed. Join us on this
+                transformative journey, and together, we will propel your
+                business to new heights of success.
+              </p>
+              <p className="mt-16 text-lg font-semibold sm:hidden block mb-10">
+                About Us:
+              </p>
+              <p className="flex sm:hidden mt-4 mx-6">
+                From advanced algorithms and powerful analytics capabilities to
+                seamless technology integration and ongoing support from our
+                trained personnel, we are dedicated to helping businesses grow
+                and succeed. Join us on this transformative journey, and
+                together, we will propel your business to new heights of
+                success.
+              </p>
+            </motion.div>
+          </div>
+        </div>
+      </div>
+      <div className="w-[70%] h-[2px] bg-gradient-to-r from-blue-400 to-violet-500"></div>
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
